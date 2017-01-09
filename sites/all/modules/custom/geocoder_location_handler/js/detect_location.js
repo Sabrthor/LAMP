4b9141c1a8ccb8a971fdd399c9275f7aba45a429
Saneesh <saneesh@kirana11.com>
2017-01-09 15:48:31 +0530

    Detect and select location fix

--- sites/all/modules/custom/geocoder_location_handler/js/detect_location.js
+++ sites/all/modules/custom/geocoder_location_handler/js/detect_location.js
@@ -2,27 +2,35 @@
   Drupal.behaviors.findLocation = {
     attach: function (context) {
       $("#edit-detect-my-location").once().click(function() {
+        console.log('First button');
         var timeoutVal = 10 * 1000 * 1000;
 
         if(!!navigator.geolocation) {
+          localStorage.setItem('local_address', null); 
           navigator.geolocation.getCurrentPosition(function(position) {
             var geocoder = new google.maps.Geocoder();
             var geolocate = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
             console.log(position.coords.latitude + ', ' + position.coords.longitude);
 
-            $.get('https://kirana11revamp.dd:8443/get_stores_by_coordinate/' + position.coords.latitude + '/' + position.coords.longitude, function( result ) {
-              console.log(result);
-            });
-
             geocoder.geocode({'latLng': geolocate}, function(results, status) {
               if (status == google.maps.GeocoderStatus.OK) {
                 var result = (results.length > 1) ? results[1] : results[0];
                 var address = get_address(result.address_components, ['sublocality_level_1', 'locality']);
 
-               console.log(address);
+                localStorage.setItem('local_address', address); 
+
+                $.get('/get_stores_by_coordinate/' + position.coords.latitude + '/' + position.coords.longitude, function( result ) {
+                  console.log(result);
+                  console.log('Local address:: ' + localStorage.getItem('local_address'));
+
+                  // Redirect to homepage if store(s) available.
+                  if (result.length > 0) {
+                    window.location.href = '/';
+                  }
+                });              
               }  
-            });
+            });                  
           }, function(error) {
             var errors = { 
               1: 'Permission denied',
@@ -39,6 +47,39 @@
 
         return false;
       });
+      $("#edit-select-location").once().click(function() {
+        var autocomplete = new google.maps.places.Autocomplete(this);
+
+        autocomplete.addListener('place_changed', function () {
+          var place = autocomplete.getPlace();
+
+          if (typeof place.geometry != 'undefined') {
+            var geocoder = new google.maps.Geocoder();
+            var geolocate = new google.maps.LatLng(place.geometry['location'].lat(), place.geometry['location'].lng());
+
+            geocoder.geocode({'latLng': geolocate}, function(results, status) {
+              if (status == google.maps.GeocoderStatus.OK) {
+                var result = (results.length > 1) ? results[1] : results[0];
+                var address = get_address(result.address_components, ['sublocality_level_1', 'locality']);
+
+                localStorage.setItem('local_address', address); 
+
+                $.get('/get_stores_by_coordinate/' + place.geometry['location'].lat() + '/' + place.geometry['location'].lng(), function( result ) {
+                  console.log(result);
+                  console.log('Local address:: ' + localStorage.getItem('local_address'));
+
+                  // Redirect to homepage if store(s) available.
+                  if (result.length > 0) {
+                    window.location.href = '/';
+                  }
+                });              
+              }  
+            });  
+          } else {
+            console.log('Not a valid location');
+          }         
+        });
+      });      
     }
   };
 
